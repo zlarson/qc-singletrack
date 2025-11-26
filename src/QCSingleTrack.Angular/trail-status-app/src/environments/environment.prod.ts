2062e5669d0c135cee372c9362f10f7e6da50229
Zach Larson <zlarson@gmail.com>
2025-11-26 14:11:27 -0600

    use dev key for now

--- src/QCSingleTrack.Angular/trail-status-app/src/environments/environment.prod.ts
+++ src/QCSingleTrack.Angular/trail-status-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://your-api-domain.azurewebsites.net/api/trails',
-  apiKey: 'your-production-api-key'
+  apiKey: 'dev-local-api-key'
 };
