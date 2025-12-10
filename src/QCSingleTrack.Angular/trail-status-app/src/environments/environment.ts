9bec1dc2a6db7bd316e97566fd77ad5033a70aae
Zach Larson <zlarson@gmail.com>
2025-12-10 11:16:09 -0600

    moved logo

--- src/QCSingleTrack.Angular/trail-status-app/src/environments/environment.ts
+++ src/QCSingleTrack.Angular/trail-status-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:60997/api/trails',
+  apiUrl: 'https://qcsingletrackapi20251126134734-dda4h2bqh7dsbcb4.centralus-01.azurewebsites.net/api/trails',
   apiKey: 'dev-local-api-key'
 };
